e03647965c97098a197d93f7b3c628bb373ca2f6
Geran1ka <roman.khoruzhy@gmail.com>
2023-12-06 13:41:29 +0300

    Написать стили к card.html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const swiperThumb = new Swiper('.gift__swiper--thumb', {
+  spaceBetween: 16,
+  slidesPerView: 5,
+  freeMode: true,
+});
+
+const swiperMain = new Swiper('.gift__swiper--card', {
+  spaceBetween: 16,
+  thumbs: {
+    swiper: swiperThumb,
+  },
+});
